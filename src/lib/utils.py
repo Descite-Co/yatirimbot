4d6fdb6ba10696623747887ddfb7fcaf35d4c523
Ömer Duran <me@omerduran.dev>
2024-09-26 10:44:55 +0200

    Fix: Resolved .gitignore related error

---
+++ src/lib/utils.py
@@ -0,0 +1,7 @@
+def get_turkish_month(month):
+    """Convert English month name to Turkish."""
+    return {
+        "January": "Ocak", "February": "Şubat", "March": "Mart", "April": "Nisan",
+        "May": "Mayıs", "June": "Haziran", "July": "Temmuz", "August": "Ağustos",
+        "September": "Eylül", "October": "Ekim", "November": "Kasım", "December": "Aralık"
+    }[month]
